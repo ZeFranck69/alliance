a07086ea2d1ba53866d8fff0f90c31dc99d18e19
Jullian Leleux <jullian@eltigre.fr>
2021-06-29 11:32:27 +0200

    Added fontsizeselect to wysiwyg

--- content/themes/eltigre/inc/class/admin/ACF.php
+++ content/themes/eltigre/inc/class/admin/ACF.php
@@ -8,6 +8,7 @@ if ( ! class_exists( 'Eltigre\Admin\ACF' ) ) {
   
       public function __construct() {
         add_filter( 'tiny_mce_before_init', array( $this,'custom_wysiwyg_colors' ) );
+        add_filter( 'acf/fields/wysiwyg/toolbars', array( $this,'customize_tinymce_toolbars' ), 999, 1 );
         add_action( 'acf/input/admin_enqueue_scripts', array( $this, 'register_scripts' ) );
       }
 
@@ -16,6 +17,20 @@ if ( ! class_exists( 'Eltigre\Admin\ACF' ) ) {
         wp_localize_script( 'acf-admin', 'acfAdminColors', COLORS );
       }
 
+
+      public static function customize_tinymce_toolbars( $toolbars ) {
+        $toolbar_1 = array();
+        foreach ( $toolbars['Full'][1] as $item ) {
+          $toolbar_1[] = $item;
+          if ( $item === 'formatselect') {
+            $toolbar_1[] = 'fontsizeselect';
+          }
+        }
+
+        $toolbars['Full'][1] = $toolbar_1;
+
+        return $toolbars;
+      }
       public static function custom_wysiwyg_colors( $init ) {
           $colors = array();
           foreach( COLORS as $hex => $color ) {
@@ -28,7 +43,7 @@ if ( ! class_exists( 'Eltigre\Admin\ACF' ) ) {
   
       $init['textcolor_map'] = '['.$textcolor_map.']';
       $init['textcolor_rows'] = 8; 
-      
+      $init['fontsize_formats'] =  '8px 10px 12px 14px 16px 20px 24px 28px 32px 36px 48px 60px 72px 96px';
       return $init;
     }
   
